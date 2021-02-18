7cf831f0b86a807a00bace076e129b610204aaa5
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-18 17:34:50 +0200

    added useRef functionality

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,18 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
   const [showLinks, setShowLinks] = useState(false)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showLinks])
   return <nav>
     <div className="nav-center">
       <div className="nav-header">
@@ -13,8 +24,8 @@ const Navbar = () => {
           <FaBars />
         </button>
       </div>
-      <div className={`${showLinks ? 'links-container show-container' : 'links-container'}`}>
-        <ul className="links">
+      <div className='links-container' ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
           {links.map((link) => {
             const { id, url, text } = link;
             return (
